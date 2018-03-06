28befb1d81c24fc2c708acfadafe4897dfb6f036
DanielTran3 <dtran3@ualberta.ca>
2018-03-06 16:05:34 -0700

    Matlab prototyping code

---
+++ OriginalSourceCode/Superposition_Test.m
@@ -0,0 +1,50 @@
+%{
+*********************************************************************************************************
+*
+*                                    MATLAB SOUND PROTOTYPE CODE
+*
+*                                         CYCLONE V SOC
+*
+* Filename      : Superposition_Test.m
+* Version       : V1.00
+* Creation 		: January 28, 2018
+* Created by 	: Mingjun Zhao (zhao2@ualberta.ca ), Daniel Tran (dtran3@ualberta.ca)
+*********************************************************************************************************
+* Note(s)       : This file shows how sounds of different frequencies can be played in Matlab, and also 
+                  shows how sounds can be superpositioned together and what that sounds like. This provides 
+                  a basis for performing superposition in the sample-based synthesizer code
+*********************************************************************************************************
+%} 
+ 
+ % Test for sounds playing at same time
+
+ % Compute Generic Sin waves of each frequency
+ clear all
+ Fs=8000;
+ Ts=1/Fs;
+ t=[0:Ts:1];
+ F_A=440; %Frequency of note A is 440 Hz
+ A=sin(2*pi*F_A*t);
+ 
+ F_B=750; %Frequency of note B is 750 Hz
+ B=sin(2*pi*F_B*t);
+ 
+ % Superposition waves
+ C=(A+B)/2;
+ 
+ % Play one note
+ sound(A,Fs);
+ pause(2);
+ 
+ % Play another note
+ sound(B,Fs);
+ pause(2);
+ 
+ % Play both ntoes at same time
+ sound(A,Fs);
+ sound(B,Fs);
+ 
+ pause(2);
+ 
+ % Play superposition of notes and determine if the same
+ sound(C,Fs);
---
+++ OriginalSourceCode/Synthesizer_Twinkle_Twinkle_Little_Star_Test.m
@@ -0,0 +1,123 @@
+%{
+*********************************************************************************************************
+*
+*                                    MATLAB PLAYING A SONG PROTOTYPE CODE
+*
+*                                         CYCLONE V SOC
+*
+* Filename      : Superposition_Test.m
+* Version       : V1.00
+* Creation 		: January 28, 2018
+* Created by 	: Mingjun Zhao (zhao2@ualberta.ca ), Daniel Tran (dtran3@ualberta.ca)
+*********************************************************************************************************
+* Note(s)       : This file is an example of what a simple song would sound like by using the same 
+                  frequencies as those for a piano. The sounds are created as sine waves and played with 
+                  a delay of 1 second between each note. This file provides a basis for a wavetable 
+                  synthesizer, if the need for one were to arrise. It shows how a sound can be created 
+                  using a simple sine wave at a certain frequency. An implementation of this code would 
+                  be similar, but would require stepping through the wave (in this, the sound function 
+                  is used instead)
+*********************************************************************************************************
+%} 
+
+% Frequency to Tone
+% A - 440Hz
+% B - 493.88Hz
+% C - 523.25Hz
+% D - 587.37Hz
+% E - 659.26Hz
+% F - 698.46Hz
+% G - 783.99Hz
+
+clear all
+
+% Compute Generic Sin waves of each frequency
+Fs=8000;
+Ts=1/Fs;
+t=[0:Ts:1];
+F_A=440; %Frequency of note A is 440 Hz
+F_B=493.88; %Frequency of note B is 493.88 Hz
+F_C=523.25; %Frequency of note C is 523.25 Hz
+F_D=587.38; %Frequency of note D is 587.37 Hz
+F_E=659.26; %Frequency of note E is 659.26 Hz
+F_F=698.46; %Frequency of note F is 698.46 Hz
+F_G=783.99; %Frequency of note G is 783.99 Hz
+A=sin(2*pi*F_A*t);
+B=sin(2*pi*F_B*t);
+C=sin(2*pi*F_C*t);
+D=sin(2*pi*F_D*t);
+E=sin(2*pi*F_E*t);
+F=sin(2*pi*F_F*t);
+G=sin(2*pi*F_G*t);
+
+% Play sound - Twinkle Twinkle Little Star
+sound(C,Fs);
+pause(1);
+sound(C,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(A,Fs);
+pause(1);
+sound(A,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(D,Fs);
+pause(1);
+sound(D,Fs);
+pause(1);
+sound(C,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(D,Fs);
+pause(1);
+sound(C,Fs);
+pause(1);
+sound(C,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(A,Fs);
+pause(1);
+sound(A,Fs);
+pause(1);
+sound(G,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(F,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(E,Fs);
+pause(1);
+sound(D,Fs);
+pause(1);
+sound(D,Fs);
+pause(1);
+sound(C,Fs);
+pause(1);
